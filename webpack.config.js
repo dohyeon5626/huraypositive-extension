495c058b84c995bf7d8dd26d0f8c59e3bb2126e4
dohyeon5626 <kodohyeon71@gmail.com>
2023-11-18 13:13:42 +0900

    [UPDATE] 기본 google oauth 설정
    #1
    google oauth, .env 파일 분리

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
@@ -27,6 +29,24 @@ module.exports = {
    plugins: [
       new CopyPlugin({
          patterns: [{from: ".", to: ".", context: "public"}]
+      }),
+      new CopyPlugin({
+         patterns: [
+            {
+              from: "config/manifest.json",
+              to: "manifest.json",
+              transform(content, path) {
+                return modifyManifest(content)
+              },
+            },
+          ],
       })
    ]
-};
+}
+
+const modifyManifest = (buffer) => {
+   const manifest = JSON.parse(buffer.toString())
+   manifest.key = process.env.KEY
+   manifest.oauth2.client_id = process.env.CLIENT_ID
+   return JSON.stringify(manifest, null, 2)
+}
